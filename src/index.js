c195053efd8b57ea1ef71d62fddf4d26e2f35112
rdineshkumarreddy <dineshkumarreddy58@gmail.com>
2026-05-28 19:49:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
-
 dotenv.config({
-    path:"./.env"
-})
+    path: "./.env"
+});
 
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 8001;
 
-connectDB( )
-.then(()=>{
-    app.listen(PORT , ()=>{
+// Root route
+app.get("/", (req, res) => {
+    res.send("Backend deployed successfully");
+});
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
         console.log(`The server is running on ${PORT}`);
-    })
+    });
 })
-
 .catch((err) => {
-    console.log("MongoDB connection error",err)
-})
-
-
+    console.log("MongoDB connection error", err);
+});
